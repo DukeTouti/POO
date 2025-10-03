88e4ac55a85837b535d76c06f301bbf308039083
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-10-03 13:34:58 +0100

    tp2, exercice5: completed

--- workspace/tp2/src/ex5/Game.java
+++ workspace/tp2/src/ex5/Game.java
@@ -1,9 +1,33 @@
 package ex5;
 
 public class Game {
+	private Guess guess;
+	private int tentativesRestantes;
 
-	public Game() {
+	public Game(Guess guess) {
 		// TODO Auto-generated constructor stub
+		this.guess = guess;
+		this.tentativesRestantes = guess.getTentativesMax();
+	}
+	
+	public String proposition(int proposition) {
+		tentativesRestantes--;
+		
+		if (proposition == guess.getNombreADeviner()) {
+			return "Félicitation ! vous venez de trouver le nombre";
+		} else if (proposition < guess.getNombreADeviner()) {
+			return "La valeur recherchée est plus grande";
+		} else {
+			return "La valeur recherchée est plus petite";
+		}
+	}
+	
+	public boolean encoreDesTentatives() {
+		return tentativesRestantes > 0;
+	}
+	
+	public int getTentativesRestantes() {
+		return tentativesRestantes;
 	}
 
 }
