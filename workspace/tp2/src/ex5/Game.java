913bcc0ecbd56cf24d416ad424fdf4d9370506b1
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-10-03 10:47:06 +0100

    tp2, exercice5: en cours

---
+++ workspace/tp2/src/ex5/Game.java
@@ -0,0 +1,9 @@
+package ex5;
+
+public class Game {
+
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ workspace/tp2/src/ex5/Guess.java
@@ -0,0 +1,9 @@
+package ex5;
+
+public class Guess {
+
+	public Guess() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
