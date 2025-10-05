ea3d87842fd8714df8fb3d58e939e150a2842713
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-10-05 19:56:14 +0100

    tp3, exercice1, const AnneeEtudiant, getters, setters: implemented

--- workspace/tp3/src/ex1/AnneeEtudiant.java
+++ workspace/tp3/src/ex1/AnneeEtudiant.java
@@ -2,10 +2,40 @@ package ex1;
 
 public class AnneeEtudiant {
 	private int idEtudiant;
+	private int nbModules;
+	private ResultatModule[] modules;
 	
+	private static final int MAX_MODULES = 20;
 
-	public AnneeEtudiant() {
+	public AnneeEtudiant(int idEtudiant) {
 		// TODO Auto-generated constructor stub
+		this.setIdEtudiant(idEtudiant);
+		this.setNbModules(0);
+		this.setModules(new ResultatModule[MAX_MODULES]);
+	}
+
+	public int getIdEtudiant() {
+		return idEtudiant;
+	}
+
+	public void setIdEtudiant(int idEtudiant) {
+		this.idEtudiant = idEtudiant;
+	}
+
+	public int getNbModules() {
+		return nbModules;
+	}
+
+	public void setNbModules(int nbModules) {
+		this.nbModules = nbModules;
+	}
+
+	public ResultatModule[] getModules() {
+		return modules;
+	}
+
+	public void setModules(ResultatModule[] modules) {
+		this.modules = modules;
 	}
 
 }
