ae819bd4e41358140d091c0d16458a9fc49c6c74
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-10-05 19:17:50 +0100

    tp3, exercice1, class ResultatModule: en cours

---
+++ workspace/tp3/src/ex1/ResultatModule.java
@@ -0,0 +1,45 @@
+package ex1;
+
+public class ResultatModule {
+	private int id;
+	private int cc;
+	private int tp;
+	private int exam;
+	
+	public ResultatModule() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCc() {
+		return cc;
+	}
+
+	public void setCc(int cc) {
+		this.cc = cc;
+	}
+
+	public int getTp() {
+		return tp;
+	}
+
+	public void setTp(int tp) {
+		this.tp = tp;
+	}
+
+	public int getExam() {
+		return exam;
+	}
+
+	public void setExam(int exam) {
+		this.exam = exam;
+	}
+
+}
