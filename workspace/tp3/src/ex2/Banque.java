eacb6be2ece99eec41cfbdc535be4ec075cc5bed
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-10-17 08:47:41 +0100

    tp3, exercice supplementaire: first commit

---
+++ workspace/tp3/src/ex2/Banque.java
@@ -0,0 +1,7 @@
+package ex2;
+
+public class Banque {
+	private int id_banque;
+	private String nom_banque;
+
+}
---
+++ workspace/tp3/src/ex2/CompteBancaire.java
@@ -0,0 +1,8 @@
+package ex2;
+
+public class CompteBancaire {
+	private int id_client;
+	private String nom_client;
+	private int solde_client;
+
+}
