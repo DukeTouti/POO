9bc93823f6a8d6957015ac479112301278f2b720
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-11-09 11:41:18 +0100

    tp5: completed

---
+++ workspace/tp5/src/ex2/Etudiant.java
@@ -0,0 +1,64 @@
+package ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.PrintStream;
+
+public class Etudiant {
+	public static PrintStream ps = System.out;
+
+	public int code;
+	public String nom, prenom, filiere;
+
+	public Etudiant() {
+		super();
+
+	}
+
+	public Etudiant(int code, String nom, String prenom, String filiere) {
+		super();
+		this.code = code;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.filiere = filiere;
+	}
+
+	@Override
+	public String toString() {
+		return "Etudiant [code = " + code + ", nom = " + nom + ", prenom = " + prenom + ", filiere = " + filiere + "]";
+	}
+
+	public static boolean recherche(List<Etudiant> liste, int code) {
+
+		for (Etudiant etudiant : liste) {
+			if (etudiant.code == code) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void affichage(List<Etudiant> liste) {
+		if (liste.isEmpty()) {
+			ps.println("La liste est vide");
+			return;
+		}
+
+		ps.println("\n===   Liste des Etudiants   ===");
+		ps.println("Nombre total : " + liste.size());
+
+		for (int i = 0; i < liste.size(); i++) {
+			ps.println("[ " + (i + 1) + " ] " + liste.get(i));
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public static List trierListe(List<Etudiant> liste) {
+		List<Etudiant> liste_triee = new ArrayList<>(liste);
+		
+		liste_triee.sort((etudiant1, etudiant2) -> Integer.compare(etudiant1.code, etudiant2.code));
+		
+		return liste_triee;
+	}
+
+}
