40c5ea4e1cb8afe83c363cb9d6ce7e0e2dbbe317
HATHOUTI Mohammed Taha <hathouti.medtaha@gmail.com>
2025-12-10 16:19:20 +0100

    projet, models, Classe Acte: implemented

---
+++ workspace/CabinetMedical/src/models/Acte.java
@@ -0,0 +1,47 @@
+package models;
+
+/* ********************************************* *
+ * Classe représentant un acte médical           *
+ * Un acte est composé d'un code NGAP            *
+ * et d'un coefficient multiplicateur            *
+ * ********************************************* */
+
+public class Acte {
+	/* Attributs */
+	private Code code; /* Code NGAP (CS, KC, etc.) */
+	private int coefficient; /* Coefficiant multiplicateur */
+	
+	/* Constructeur */
+	public Acte(Code code, int coefficient) {
+		this.code = code;
+		this.coefficient = coefficient;
+	}
+	
+	/* ========== GETTERS & SETTERS ========== */
+
+	public Code getCode() {
+		return code;
+	}
+
+	public void setCode(Code code) {
+		this.code = code;
+	}
+
+	public int getCoefficiant() {
+		return coefficient;
+	}
+
+	public void setCoefficiant(int coefficient) {
+		this.coefficient = coefficient;
+	}
+	
+	/* ========== MÉTHODES ========== */
+	public double cout() {
+		return code.calculerCout(this.coefficient);
+	}
+	
+	@Override
+	public String toString() {
+		return code.name() + " x" + coefficient + " -> " + String.format("%.2f", cout()) + "€";
+	}
+}
